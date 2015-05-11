9cbf247c114379a53ff6cb4b82f56cccf3d0d47d
ROSS LIBBY <libtheross@gmail.com>
2015-05-11 12:43:15 -0400

    fixed 	modified:   js/form.js

--- js/form.js
+++ js/form.js
@@ -9,41 +9,41 @@
 
 $(document).on("ready",function() {
 
-    $("#email-form [type='submit']").click(function(event) {
-        event.preventDefault();
-        //get input field values
-        var user_name       = $('input[name=name]').val()
-        var user_email      = $('input[name=email]').val()
+    // $("#email-form [type='submit']").click(function(event) {
+    //     event.preventDefault();
+    //     //get input field values
+    //     var user_name       = $('input[name=name]').val()
+    //     var user_email      = $('input[name=email]').val()
        
-        //data to be sent to server
-        post_data = {'userName':user_name, 'userEmail':user_email, 'userSubject':user_subject, 'userMessage':user_message}
+    //     //data to be sent to server
+    //     post_data = {'userName':user_name, 'userEmail':user_email, 'userSubject':user_subject, 'userMessage':user_message}
        
-        //Ajax post data to server
-        $.post('sendmail.php', post_data, function(response){  
+    //     //Ajax post data to server
+    //     $.post('sendmail.php', post_data, function(response){  
            
-            //load json data from server and output message    
-            if(response.type == 'error') {
+    //         //load json data from server and output message    
+    //         if(response.type == 'error') {
 
-                output = '<div class="error-message"><p class="from">'+response.text+'</p></div>'
+    //             output = '<div class="error-message"><p class="from">'+response.text+'</p></div>'
                 
-            } else {
+    //         } else {
            
-                output = '<div class="success-message"><p class="seuccses">'+response.text+'</p></div>'
+    //             output = '<div class="success-message"><p class="seuccses">'+response.text+'</p></div>'
                
-                //reset values in all input fields
-                $('#email-form input').val('')
-                $('#email-form textarea').val('')
-            }
+    //             //reset values in all input fields
+    //             $('#email-form input').val('')
+    //             $('#email-form textarea').val('')
+    //         }
            
-            $("#result").hide().html(output).slideDown()
+    //         $("#result").hide().html(output).slideDown()
 
-        }, 'json')
+    //     }, 'json')
 
-    });
+    // });
    
-    //reset previously set border colors and hide all message on .keyup()
-    $("#email-form input, #email-form textarea").keyup(function() {
-        $("#result").slideUp()
-    })
+    // //reset previously set border colors and hide all message on .keyup()
+    // $("#email-form input, #email-form textarea").keyup(function() {
+    //     $("#result").slideUp()
+    // })
    
 });
