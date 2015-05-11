694d1227494c3d3d94454c434412c96c59f81347
ROSS LIBBY <libtheross@gmail.com>
2015-05-11 10:28:05 -0400

    filled slider with images started to change slider appearance 	modified:   css/style.css 	modified:   includes/slider.php

--- includes/slider.php
+++ includes/slider.php
@@ -1,87 +1,21 @@
 <div class="w-slide">
 				<div class="w-container">
 					<div class="w-row row-slider">
-						<div class="w-col w-col-6 w-col-stack">
-							<div id="change-tp" class="bslider-txt-wrapper">
-								<h1 class="slid-bus-txt" data-ix="zom-in-out-txt-slider"><span class="white">WELCOME TO Infoworkz.</span></h1>
-								<div class="slider-sub-text">
-									<div data-ix="move-from-bottom-txt-slider">The perfect ThemeForest template to get you started.</div>
-								</div>
-								<div class="space" data-ix="move-from-left-txt-slider">
-									<p class="white">Vivamus elementum semper nisi. Duis lobortis massa imperdiet quam. Etiam sit amet orci eget eros faucibus tincidunt. Sed fringilla mauris sit amet nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
-								</div>
-								<div class="space x1">
-									<a class="w-clearfix w-inline-block button btn-line btn-small" href="#" data-ix="zoom-scale-button-slider">
-										<div class="btn-ico">
-											<i class="fa fa-rocket"></i>
-										</div>
-										<div class="btn-txt">take a tour</div>
-									</a>
-									<a class="w-clearfix w-inline-block button btn-small btn-green-color" href="#" data-ix="zoom-scale-button-slider-2">
-										<div class="btn-txt">Buy apital now</div>
-									</a>
-								</div>
-							</div>
-						</div>
-						<div class="w-col w-col-6 w-col-stack col-center">
-							<div class="w-hidden-medium w-hidden-small w-hidden-tiny" data-ix="move-mascot-slide"><img src="images/bussines-man.png" alt="bussines-man.png">
-							</div>
-						</div>
+						<img src="images/pages/website-design-web-development.jpg" alt="Infoworkz Web Design &amp; Development" />
 					</div>
 				</div>
 			</div>
 			<div class="w-slide">
 				<div class="w-container">
 					<div class="w-row row-slider">
-						<div class="w-col w-col-6 w-col-stack w-hidden-small w-hidden-tiny col-center">
-							<div class="w-hidden-medium w-hidden-small w-hidden-tiny" data-ix="move-mascot-from-bottom-top"><img src="images/bussines-man2.png" width="782" alt="bussines-man2.png">
-							</div>
-						</div>
-						<div class="w-col w-col-6 w-col-stack">
-							<div id="change-tp-a" class="bslider-txt-wrapper">
-								<h1 class="slid-bus-txt" data-ix="move-from-bottom-txt-slider-2"><span class="white">I'M BORED WITH the CODE!</span></h1>
-								<div class="slider-sub-text">
-									<div data-ix="zom-in-out-txt-slider-2">We'll help you to make the perfect website for you business.</div>
-								</div>
-								<div class="space">
-									<p class="white" data-ix="zoom-scale-button-slider-3">Fusce pretium augue vel aliquam fringilla. Mauris varius nunc in magna ultricies, vel auctor massa ornare. In lacinia velit nec dolor dhe dapibus. Phasellus cursus aliquam nulla, nec eleifend mauris dignissim non. Nunc semper congue nunc at viverra.</p>
-								</div>
-								<div class="space x1">
-									<a class="w-clearfix w-inline-block button btn-small btn-dark-color" href="#" data-ix="zoom-scale-button-slider">
-										<div class="btn-txt">Try Now!</div>
-									</a>
-								</div>
-							</div>
-						</div>
+						<img src="images/pages/website-design-web-development.jpg" alt="Infoworkz Web Design &amp; Development" />
 					</div>
 				</div>
 			</div>
 			<div class="w-slide">
 				<div class="w-container">
 					<div class="w-row row-slider">
-						<div class="w-col w-col-6 w-col-stack">
-							<div id="change-tp-b" class="bslider-txt-wrapper">
-								<h1 class="slid-bus-txt" data-ix="move-from-bottom-txt-slider-3"><span class="white">you will love apital!</span></h1>
-								<div class="slider-sub-text">
-									<div data-ix="move-txt-from-right">Let your creative juices flow!</div>
-								</div>
-								<div class="space" data-ix="zom-in-out-txt-slider-3">
-									<p class="white">Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. Aenean viverra rhoncus pede. Curabitur turpis. Pellentesque dapibus hendrerit tortor. Etiam vitae tortor.</p>
-								</div>
-								<div class="space x1">
-									<a class="w-clearfix w-inline-block button btn-small btn-gradient wow rot" href="#" data-ix="move-from-left-txt-slider-2">
-										<div class="btn-ico">
-											<i class="fa fa-leaf"></i>
-										</div>
-										<div class="btn-txt">what u are waiting for?</div>
-									</a>
-								</div>
-							</div>
-						</div>
-						<div class="w-col w-col-6 w-col-stack w-hidden-small w-hidden-tiny col-center">
-							<div class="w-hidden-medium w-hidden-small w-hidden-tiny"><img src="images/bussines-man3.png" width="650" alt="bussines-man3.png">
-							</div>
-						</div>
+						<img src="images/pages/website-design-web-development.jpg" alt="Infoworkz Web Design &amp; Development" />
 					</div>
 				</div>
 			</div>
