752ddb8d9fa499e231f2454fa374d775119b3f5c
ROSS LIBBY <libtheross@gmail.com>
2015-05-11 10:33:40 -0400

    Configured slider 	modified:   includes/slider.php

--- includes/slider.php
+++ includes/slider.php
@@ -1,21 +1,21 @@
 <div class="w-slide">
 				<div class="w-container">
 					<div class="w-row row-slider">
-						<img src="images/pages/website-design-web-development.jpg" alt="Infoworkz Web Design &amp; Development" />
+						<img src="images/pages/website-design-web-development.jpg" alt="Web Design &amp; Development" />
 					</div>
 				</div>
 			</div>
 			<div class="w-slide">
 				<div class="w-container">
 					<div class="w-row row-slider">
-						<img src="images/pages/website-design-web-development.jpg" alt="Infoworkz Web Design &amp; Development" />
+						<img src="images/pages/web-hosting.jpg" alt="Servers &amp; Hosting" />
 					</div>
 				</div>
 			</div>
 			<div class="w-slide">
 				<div class="w-container">
 					<div class="w-row row-slider">
-						<img src="images/pages/website-design-web-development.jpg" alt="Infoworkz Web Design &amp; Development" />
+						<img src="images/pages/support-services.jpg" alt="IT Support Services" />
 					</div>
 				</div>
 			</div>
